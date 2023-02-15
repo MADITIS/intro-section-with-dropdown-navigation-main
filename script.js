e3295a06d62e6a09f68ae447935789bdc965c248
MDS <manik6418@gmail.com>
2023-02-15 18:24:21 +0530

    first test

---
+++ script.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector(".burger-menu")
+const closeMenu = document.querySelector(".close-menu")
+// console.log(menu)
+window.addEventListener("click", ()=> {
+    const sideMenu = document.querySelector(".side-bar")
+    // sideMenu.classList.remove("show")
+
+})
+menu.addEventListener('click', addSideBar)
+closeMenu.addEventListener("click", addSideBar)
+function addSideBar() {
+    const sideMenu = document.querySelector(".side-bar")
+    sideMenu.classList.toggle("show")
+}
