a5c15d680914f125a51c0ff6f5de7dcc4e33ca00
MDS <manik6418@gmail.com>
2023-02-16 20:36:33 +0530

    improved menu

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const menu = document.querySelector(".burger-menu")
 const closeMenu = document.querySelector(".close-menu")
-const dropMenu = document.querySelectorAll(".row.drop-menu")
-const sideMenu = document.querySelectorAll(".row.side-menu")
+const dropMenu = document.querySelectorAll("drop-menu")
+const sideMenu = document.querySelectorAll(".side-menu")
+const listElements = document.querySelectorAll(".list")
 // console.log(menu)
 window.addEventListener("click", ()=> {
     const sideMenu = document.querySelector(".side-bar")
@@ -20,95 +21,67 @@ sideMenu.forEach((item) => {
     item.addEventListener("click", showDropMenu)
 })
 
-// function showSideMenu(event) {
-//     let target = event.currentTarget
-
-// }
-
 
 dropMenu.forEach((item) => {
     item.addEventListener("pointerover", showDropMenu)
+    item.addEventListener("pointerout", removeDropMenu)
+    
 })
+
+function removeDropMenu(e) {
+    let target = e.currentTarget
+    if (target.classList.contains("features")) {
+        const featuresList = target.querySelector(".features-list")
+        if (featuresList.classList.contains("toggle")) {
+            featuresList.classList.remove("toggle")
+            target.querySelector(".arrow").src = "./images/icon-arrow-down.svg"
+        }
+        
+    }else if (target.classList.contains("company")) {
+        const companyList = target.querySelector(".company-list") 
+        if (companyList.classList.contains("toggle")) {
+            companyList.classList.remove("toggle")
+            target.querySelector(".arrow").src = "./images/icon-arrow-down.svg"
+        }
+    }
+}
 
 function showDropMenu(event) {
     console.log(event.target, event.currentTarget)
     let target = event.currentTarget
     if (target.classList.contains("features")) {
-        // let featuresList = document.querySelector(".features-list")
-        // let cord = target.getBoundingClientRect()
-        // console.log(featuresList, cord)
-        // let left = cord.left- featuresList.offsetWidth / 2 + "px"
-        // let top = cord.top + target.offsetHeight + 10 + "px"
-        // featuresList.style.left = left
-        // featuresList.style.top = top
-        let element = document.querySelector(".features-list")
+        let element = target.querySelector(".features-list")
         if (event.type === "click") {
             showSideMenu(target, element)
             return
         }
+        element.classList.add("toggle")
+        target.querySelector(".arrow").src = "./images/icon-arrow-up.svg"
 
-        move(element, target)
-        // if (element.classList.contains("toggle")) {
-            // console.
-        // } else {
-        //     element.classList.toggle("toggle")
-        // }
     } else if (target.classList.contains("company")) {
-        // move(".company-list", target)
-        let element = document.querySelector(".company-list")
+        let element = target.querySelector(".company-list")
         if (event.type === "click") {
             showSideMenu(target, element)
             return
         }
-        move(element, target)
-        // if (element.classList.contains("toggle")) {
-        // } else {
-        //     element.classList.toggle("toggle")
-        // }
+        element.classList.add("toggle")
+        target.querySelector(".arrow").src = "./images/icon-arrow-up.svg"
     }
 
 
 }
 
 function showSideMenu(target, element) {
-    let elements = document.querySelectorAll(".list")
-    console.log("running so")
-    elements.forEach((element) => {
-        if (element.classList.contains("toggle")) {
-            // element.classList.remove("toggle")
-            element.classList.toggle("toggle")
-            console.log("no menu", element)
-            target.querySelector(".arrow").src = "./images/icon-arrow-down.svg"
-        } else {
-            // console.log("menu")
-            console.log("menu", element)
-            target.querySelector(".arrow").src = "./images/icon-arrow-up.svg"
-            const cord = target.getBoundingClientRect()
-            console.log("Appending")
-            let left = cord.left- element.offsetWidth / 2 + "px"
-            let top = cord.top + target.offsetHeight + 10 + "px"
-            element.style.left = left
-            element.style.top = top
-            // element.style.display = "block"
-            element.style.position = "fixed"
-            element.classList.toggle("toggle")
-        }
-    })
-    // target.append(element)
-   
+    if (element.classList.contains("toggle")) {
+        element.classList.remove("toggle")
+        target.querySelector(".arrow").src = "./images/icon-arrow-down.svg"
+    } else {
+        element.classList.add("toggle")
+        // element.style.top = "15rem"
+        element.style.position = "relative"
+        element.style.top = "0"
 
-}
-
-function move(element, target) {
-    console.log(element)
-    let cord = target.getBoundingClientRect()
-    // console.log(featuresList, cord)
-    let left = cord.left- element.offsetWidth / 2 + "px"
-    let top = cord.top + target.offsetHeight + 10 + "px"
-    element.style.left = left
-    element.style.top = top
-    // element.style.display = "block"
-    element.classList.toggle("toggle")
-    target.querySelector(".arrow").src = "./images/icon-arrow-up.svg"
+        target.querySelector(".arrow").src = "./images/icon-arrow-up.svg"
+    }
 
 }
